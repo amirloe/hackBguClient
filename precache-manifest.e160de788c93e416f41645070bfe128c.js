c56a7cea9e79de05fb0fe5cabc4eb99032725141
amirloe <amirloe@post.bgu.ac.il>
2020-09-15 10:21:31 +0300

    Updates

---
+++ precache-manifest.e160de788c93e416f41645070bfe128c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be45fea50646a1b8c7ceb31ca97ad45a",
+    "url": "/hackBguClient/index.html"
+  },
+  {
+    "revision": "15ffcbefa23fcf89fd91",
+    "url": "/hackBguClient/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8ca8f9c6a890d26d64fa",
+    "url": "/hackBguClient/static/css/main.38502d14.chunk.css"
+  },
+  {
+    "revision": "15ffcbefa23fcf89fd91",
+    "url": "/hackBguClient/static/js/2.53e42850.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hackBguClient/static/js/2.53e42850.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ca8f9c6a890d26d64fa",
+    "url": "/hackBguClient/static/js/main.0489bf5c.chunk.js"
+  },
+  {
+    "revision": "973bf06556da2ca2788e",
+    "url": "/hackBguClient/static/js/runtime-main.9e045d65.js"
+  },
+  {
+    "revision": "e431bd55e6e0bda4e6473c2c9d6812f3",
+    "url": "/hackBguClient/static/media/zoomate.e431bd55.jpeg"
+  }
+]);
